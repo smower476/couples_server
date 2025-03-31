d097cb5058eee74c2334af45a1e3db895c11f20a
Anton <45099803+MrAntonS@users.noreply.github.com>
2025-03-31 19:34:38 -0400

    Deleted bad code(Max moment)

--- src/regex.cpp
+++ src/regex.cpp
@@ -11,8 +11,3 @@ bool is_valid_password(const std::string& password) {
     std::regex password_regex("^[a-zA-Z0-9@#%*!?]{8,32}$");
     return std::regex_match(password, password_regex);
 }
-
-bool is_valid_jwt(const std::string& token) {
-    std::regex jwt_regex(R"(^[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+$)");
-    return std::regex_match(token, jwt_regex);
-}
